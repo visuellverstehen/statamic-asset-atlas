63fc32aece3239a0bd5662116e488559ad2deb77
Simon Schweissinger <dev@schweissinger.co.uk>
2025-09-22 14:02:07 +0100

    feat: add dedicated replace test (skipped for now)

--- tests/Feature/AssetTracking/TopLevel/AssetsFieldTest.php
+++ tests/Feature/AssetTracking/TopLevel/AssetsFieldTest.php
@@ -27,3 +27,22 @@ it('tracks top-level assets field references', function () {
 
     expect($entry)->not->toBeTrackedFor($asset2);
 });
+
+it('updates references when replacing data', function () {
+    $asset = $this->createAsset('test-assets-field.jpg');
+    $asset->save();
+
+    $entry = $this->createEntryWithTopLevelAsset('assets_field', [$asset->path()]);
+    $entry->save();
+
+    expect($entry)->toBeTrackedFor($asset);
+
+    $asset2 = $this->createAsset('test-assets-field-2.jpg');
+    $asset2->save();
+
+    $entry->set('assets_field', [$asset2->path()]);
+    $entry->save();
+
+    expect($entry)->not->toBeTrackedFor($asset);
+    expect($entry)->toBeTrackedFor($asset2);
+})->skip();
